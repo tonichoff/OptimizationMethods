923ca4f3e599f60b2563e4ee43a978091182fbce
Anton Podchuvalov <podchuvalovvl@gmail.com>
2019-12-09 19:56:38 +1000

    End Gradient and Newton Method

--- Program.cs
+++ Program.cs
@@ -1,13 +1,18 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using MathNet.Numerics.LinearAlgebra;
 
-namespace Methods
+namespace lab1
 {
     class Program
     {
         const double Error = 10e-5;
-        const double MaxSteps = 500;
+        const double MaxSteps = 100;
+        const int n = 6;
+        static readonly Vector<double> EV = Vector<double>.Build.Dense(n, Error);
 
         static double Function(Vector<double> x, Matrix<double> A, Vector<double> b)
         {
@@ -27,47 +32,91 @@ namespace Methods
         static Vector<double> NewtonMethod(Vector<double> x0, Matrix<double> A, Vector<double> b)
         {
             var x = x0;
-            for (int i = 0; i < MaxSteps; ++i)
-            {
-                var xk = x - SD(A).Inverse() * FD(x, A, b);
-                if ((xk - x).L2Norm() < Error)
-                {
-                    Console.WriteLine($"Newton method stoped work on {i} iteration");
-                    return xk;
-                }
-                xk.CopyTo(x);
-            }
-            Console.WriteLine($"Newton method stoped work on {MaxSteps - 1} iteration");
+            var xk = x - SD(A).Inverse() * FD(x, A, b);
+            xk.CopyTo(x);
             return x;
+        }
+
+        static bool Less(Vector<double> a, Vector<double> b)
+        {
+            for (var i = 0; i < n; ++i)
+            {
+                if (a[i] < b[i])
+                {
+                    return true;
+                }
+                else if (a[i] > b[i])
+                {
+                    return false;
+                }
+            }
+            return false;
+        }
+
+        static Vector<double> ABS(Vector<double> a)
+        {
+            for (var i = 0; i < n; ++i)
+            {
+                a[i] = Math.Abs(a[i]);
+            }
+            return a;
+        }
+
+        static double ModGrad(Vector<double> x)
+        {
+            var result = 0.0;
+            foreach (var xk in x)
+            {
+                result += xk * xk;
+            }
+            return Math.Sqrt(result);
         }
 
         static Vector<double> GradientMethod(Vector<double> x0, Matrix<double> A, Vector<double> b)
         {
             var x = x0;
-            var l = 10e-4;
+            double l = ((double)1 / 60);
             for (int i = 0; i < MaxSteps; ++i)
             {
-                var xk = x - l * FD(x, A, b);
-                if ((xk - x).L2Norm() < Error)
+                var fd = FD(x, A, b);
+                var xk = x - l * fd;
+                if ((xk - x).L2Norm() <= Error)
                 {
-                    Console.WriteLine($"Gradient method stoped work on {i} iteration");
+                    Console.WriteLine($"Gradient method stoped work on {i + 1} iteration");
                     return xk;
                 }
+                Console.WriteLine($"Gradient. Step {i + 1}. x = {xk}");
                 xk.CopyTo(x);
             }
-            Console.WriteLine($"Gradient method stoped work on {MaxSteps - 1} iteration");
+            Console.WriteLine($"Gradient method stoped work on {MaxSteps} iteration");
             return x;
         }
 
         static void Main(string[] args)
         {
-            var n = 6;
             var x = Vector<double>.Build.Dense(n, 1);
             var A = Matrix<double>.Build.RandomPositiveDefinite(n);
+            Console.WriteLine(A);
+            Console.ReadKey();
             var b = Vector<double>.Build.Random(n);
 
-            Console.WriteLine(NewtonMethod(x, A, b));
-            Console.WriteLine(GradientMethod(x, A, b));
+            var watch1 = System.Diagnostics.Stopwatch.StartNew();
+            var newtonResult = NewtonMethod(x, A, b);
+            watch1.Stop();
+            Console.WriteLine(newtonResult);
+            Console.WriteLine($"Метод Ньютона отработал за {watch1.ElapsedMilliseconds} милисекунд");
+
+            Console.WriteLine(A);
+            Console.ReadKey();
+
+            var watch2 = System.Diagnostics.Stopwatch.StartNew();
+            var gradientResult = GradientMethod(x, A, b);
+            watch2.Stop();
+            Console.WriteLine(gradientResult);
+            Console.WriteLine($"Градиентный метод отработал за {watch2.ElapsedMilliseconds} милисекунд");
+
+            Console.WriteLine("Идеальный резльтат");
+            Console.WriteLine((-1) * A.Inverse() * b);
 
             Console.ReadKey();
         }
