fa47d69cc14f87234182d06ec01c1a5c1653bef7
Anton Podchuvalov <podchuvalovvl@gmail.com>
2019-12-23 22:30:08 +1000

    Implemate someone solution

--- fifth_lab/SimplexMethod/SimplexMethod/Program.cs
+++ fifth_lab/SimplexMethod/SimplexMethod/Program.cs
@@ -6,7 +6,28 @@ namespace SimplexMethod
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double[,] table = { {25, -3,  5},
+                                {30, -2,  5},
+                                {10,  1,  0},
+                                { 6,  3, -8},
+                                { 0, -6, -5} };
+            double[] result = new double[2];
+            double[,] tableResult;
+            SimplexMethod method = new SimplexMethod(table);
+            tableResult = method.Calculate(result);
+            Console.WriteLine("Решенная симплекс-таблица:");
+            for (int i = 0; i < tableResult.GetLength(0); i++)
+            {
+                for (int j = 0; j < tableResult.GetLength(1); j++)
+                {
+                    Console.Write(tableResult[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("Решение:");
+            Console.WriteLine("X[1] = " + result[0]);
+            Console.WriteLine("X[2] = " + result[1]);
+            Console.ReadLine();
         }
     }
 }
---
+++ fifth_lab/SimplexMethod/SimplexMethod/SimplexMethod.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SimplexMethod
+{
+    // https://vscode.ru/prog-lessons/simpleks-metod-realizatsiya.htmls
+    class SimplexMethod
+    {
+        double[,] table;
+
+        int m;
+        int n;
+
+        List<int> basis;
+
+        public SimplexMethod(double[,] sourceTable)
+        {
+            m = sourceTable.GetLength(0);
+            n = sourceTable.GetLength(1);
+            table = new double[m, n + m - 1];
+            basis = new List<int>();
+            for (int i = 0; i < m; ++i)
+            {
+                for (int j = 0; j < table.GetLength(1); ++j)
+                {
+                    if (j < n)
+                    {
+                        table[i, j] = sourceTable[i, j];
+                    }
+                    else
+                    {
+                        table[i, j] = 0;
+                    }
+                }
+                if ((n + i) < table.GetLength(1))
+                {
+                    table[i, n + i] = 1;
+                    basis.Add(n + i);
+                }
+            }
+            n = table.GetLength(1);
+        }
+
+        public double[,] Calculate(double[] result)
+        {
+            int mainCol, mainRow;
+            while (!IsItEnd())
+            {
+                mainCol = FindMainCol();
+                mainRow = FindMainRow(mainCol);
+                basis[mainRow] = mainCol;
+                double[,] newTable = new double[m, n];
+                for (int j = 0; j < n; ++j)
+                {
+                    newTable[mainRow, j] = table[mainRow, j] / table[mainRow, mainCol];
+                }
+                for (int i = 0; i < m; ++i)
+                {
+                    if (i == mainRow)
+                    {
+                        continue;
+                    }
+                    for (int j = 0; j < n; ++j)
+                    {
+                        newTable[i, j] = table[i, j] - table[i, mainCol] * newTable[mainRow, j];
+                    }
+                }
+                table = newTable;
+            }
+            for (int i = 0; i < result.Length; ++i)
+            {
+                int k = basis.IndexOf(i + 1);
+                result[i] = (k != -1) ? table[k, 0] : 0;
+            }
+            return table;
+        }
+
+        private bool IsItEnd()
+        {
+            bool flag = true;
+            for (int j = 1; j < n; ++j)
+            {
+                if (table[m - 1, j] < 0)
+                {
+                    flag = false;
+                    break;
+                }
+            }
+            return flag;
+        }
+
+        private int FindMainCol()
+        {
+            int mainCol = 1;
+            for (int j = 2; j < n; ++j)
+            {
+                if (table[m - 1, j] < table[m - 1, mainCol])
+                {
+                    mainCol = j;
+                }
+            }
+            return mainCol;
+        }
+
+        private int FindMainRow(int mainCol)
+        {
+            int mainRow = 0;
+            for (int i = 0; i < m - 1; ++i)
+            {
+                if (table[i, mainCol] > 0)
+                {
+                    mainRow = i;
+                    break;
+                }
+            }
+            for (int i = mainRow + 1; i < m - 1; ++i)
+            {
+                if ((table[i, mainCol] > 0) && ((table[i, 0] / table[i, mainCol]) < (table[mainRow, 0] / table[mainRow, mainCol])))
+                {
+                    mainRow = i;
+                }
+            }
+            return mainRow;
+        }
+    }
+}
